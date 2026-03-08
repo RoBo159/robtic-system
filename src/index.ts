8909ce83add1a2bab652f53ed8bda814a16a2573
RoBoCRAFTYT01 <fa83734@gmail.com>
2026-03-08 18:22:49 +0100

    remove api

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import { ClientManager } from "@core/ClientManager";
 import { connectDatabase } from "@database/connection";
-import { startApiServer } from "./api/server";
 import { Logger } from "@core/libs";
 
 await connectDatabase(process.env.MONGODB_URI!);
-
-startApiServer();
 
 const manager = ClientManager.getInstance();
 await manager.startAll().then(() => {
     Logger.success("All bots initialized.");
 })
-
